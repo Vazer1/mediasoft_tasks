5067c77224e998b649ed1e1ddc5d2ddfcd97a57d
Vazer1 <artem.viryaskin@gmail.com>
2026-06-19 19:59:39 +0400

    task1

---
+++ 2/main.go
@@ -0,0 +1,68 @@
+package main
+
+import "fmt"
+
+type Employee struct {
+	Name     string // имя
+	Age      int    // возраст
+	Position string // позиция
+	Salary   int    // зарплата
+}
+
+var commands = `
+1 - Добавить нового сотрудника
+2 - Удалить сотрудника
+3 - Вывести список сотрудников
+4 - Выйти из программы
+`
+
+func main() {
+	const size = 512
+	empls := [size]*Employee{}
+	for {
+		cmd := 0
+		fmt.Print(commands)
+		fmt.Scan(&cmd)
+
+		switch cmd {
+		case 1:
+			// Добавляем нового сотрудника
+			empl := new(Employee)
+			fmt.Println("\nИмя:")
+			fmt.Scan(&empl.Name)
+			fmt.Println("Возраст:")
+			fmt.Scan(&empl.Age)
+			fmt.Println("Позиция:")
+			fmt.Scan(&empl.Position)
+			fmt.Println("Зарплата:")
+			fmt.Scan(&empl.Salary)
+			for i := 0; i < size; i++ {
+				if empls[i] == nil {
+					empls[i] = empl
+					break
+				}
+			}
+		case 2:
+			deleteNum := 0
+			fmt.Println("Введите индекс сотрудника для удаления")
+			fmt.Scan(&deleteNum)
+			for deleteNum>=size || deleteNum<0 {
+				fmt.Println("Введите индекс сотрудника для удаления")
+				fmt.Scan(&deleteNum)
+			}
+			
+			if empls[deleteNum]!=nil{
+				empls[deleteNum] = nil
+			}
+		case 3:
+			for i := 0; i < size; i++ {
+				if empls[i] != nil {
+					fmt.Print(i)
+					fmt.Println(*empls[i])
+				}
+			}
+		case 4:
+			return
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module mediasoft_task1
+module github.com/Vazer1/mediasoft_task1
 
 go 1.23.0
